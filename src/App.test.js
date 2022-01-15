333dfcdd29aaeec40316cc8eac35a6ac17d715e4
dante01yoon <bunggl@naver.com>
2022-01-15 18:24:51 +0900

    Fix: testing

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/안녕하세요 도커 강의입니다. ㅎㅎㅎ/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test("something...", () => {
-  
-})
